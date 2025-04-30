6a54c039d46c3f9b9ba0d25485fdf3ef74d44d12
Petros Dhespollari <info@peterdsp.dev>
2025-04-30 12:08:17 +0300

    Initial Commit

---
+++ imjaDNS/imjaDNSApp.swift
@@ -0,0 +1,17 @@
+//
+//  imjaDNSApp.swift
+//  imjaDNS
+//
+//  Created by Petros Dhespollari on 30/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct imjaDNSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ imjaDNSTests/imjaDNSTests.swift
@@ -0,0 +1,16 @@
+//
+//  imjaDNSTests.swift
+//  imjaDNSTests
+//
+//  Created by Petros Dhespollari on 30/04/2025.
+//
+
+import Testing
+
+struct imjaDNSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
